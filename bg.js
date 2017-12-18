c0fa90270aff11425ca65362cb05324cbec33f49
Umiiii <baka999999999@vip.qq.com>
2017-12-18 12:21:21 +0800

    Commit

---
+++ bg.js
@@ -0,0 +1,57 @@
+
+function getPrice(fSymbol,tSymbol,t){
+	unixtime = new Date(t).getTime();
+	api = "https://min-api.cryptocompare.com/data/pricehistorical"
+	data = {"fsym":fSymbol,"tsyms":tSymbol,"ts":unixtime/1000};
+	r = ""
+
+	$.ajax({
+		type:"GET",
+		url:api,
+		data:data,
+		async:false,
+		success:function(result){
+			r = result;
+		}
+	});
+	console.log(r);
+	return r[fSymbol][tSymbol];
+}
+
+tmp = "";
+setInterval(
+		function(){
+			if (tmp===$("tr.ng-scope")[0].cells[0].innerText) return;
+			tmp = $("tr.ng-scope")[0].cells[0].innerText;
+			$("tr.ng-scope").each(
+				function(index,item){
+					tradeDate = item.cells[0].innerText;
+					pairCell = item.cells[1];
+					priceCell = item.cells[3];
+					feeCell = item.cells[5];
+					totalCell = item.cells[6];
+					pair = pairCell.innerText;
+					price = priceCell.innerText;
+					fee = feeCell.innerText;
+					total = totalCell.innerText;
+					pairDsymbol = pair.split("/")[1]
+					priceDsymbol = getPrice(pairDsymbol,"USD",tradeDate);
+					BNBPrice = getPrice("BNB","USD",tradeDate);
+					if (pairDsymbol === "BTC") {
+						BTCPrice = priceDsymbol;
+					} else {
+						BTCPrice = getPrice("BTC","USD",tradeDate);
+					}
+					feePrice = BNBPrice*fee.split(" ")[0];
+					p = price*priceDsymbol;
+					totalPrice = BTCPrice*total.split(" ")[0];
+					// priceCell.innerText = price + "*$"+priceDsymbol+"=$"+p.toFixed(2);
+					// feeCell.innerText = price + "*$" + BNBPrice +"=$"+feePrice.toFixed(2);
+					priceCell.innerText = "$"+p.toFixed(2);
+					feeCell.innerText ="$"+feePrice.toFixed(2);
+					totalCell.innerText = "$"+totalPrice.toFixed(2);
+
+				}
+			)
+		},
+		2000);
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Binance History Helper
+This is an extension for Google Chrome in Binance used to display the history USD price in trade pair.
+To use this extension at your own risk, no warranty.
+
+
+# How to install
+open `chrome://extensions/` , check the `developer mode`, and then load this extension from folder.
+
+# Notice
+Entering history page may freeze for a little while, I'll be happy if someone want to solve it.
